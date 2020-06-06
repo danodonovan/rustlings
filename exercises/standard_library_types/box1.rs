8eb7743fbaf120315ccd479a9ddfb7b1e021a20f
Dan O'Donovan <dan.odonovan@gmail.com>
2020-06-06 23:46:47 +0100

    Another rust session

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -20,7 +20,7 @@
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -30,7 +30,7 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    unimplemented!()
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
