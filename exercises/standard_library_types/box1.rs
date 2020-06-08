560da0fe6ab45ae4b3ac45d5c7952a38a27fc504
Dan O'Donovan <dan.odonovan@gmail.com>
2020-06-08 23:42:13 +0100

    More learnz

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,8 +16,6 @@
 //
 // Execute `rustlings hint box1` for hints :)
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
     Cons(i32, Box<List>),
@@ -34,7 +32,7 @@ pub fn create_empty_list() -> List {
 }
 
 pub fn create_non_empty_list() -> List {
-    unimplemented!()
+    List::Cons(0, Box::new(List::Nil))
 }
 
 #[cfg(test)]
